052e8f306393c44054763a2ac9ea97755d97b50e
Pana MeRang <panamerang@pc.ikvu.vn>
2025-09-21 21:23:43 +0700

    rca: state

--- rca.h
+++ rca.h
@@ -4,4 +4,40 @@
 #ifndef RCA_H
 #define RCA_H
 
+typedef struct rca_log {
+	int text_size;
+	char *text;
+	int term;
+	int index;
+} rca_log;
+
+typedef struct rca_state {
+	int current_term;
+	int voted_for;
+	struct {
+		int log_size;
+		rca_log *log;
+	};
+	
+	int commit_index;
+	int last_applied;
+
+
+	int server_count;
+	int *next_index;
+	int *match_index;	
+} rca_state;
+
+typedef struct rca_request_vote_in {
+} rca_request_vote_in;
+
+typedef struct rca_request_vote_ou {
+} rca_request_vote_ou;
+
+typedef struct rca_append_entries_in {
+} rca_append_entries_in;
+
+int rca_process_request_vote();
+int rca_process_append_entries();
+
 #endif
